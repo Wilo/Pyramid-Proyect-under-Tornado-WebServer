4ed67c1a8ffcc4596613fc4aa34248d8eb78ec99
William Mendez <wmendezc19@gmail.com>
2014-07-16 16:46:19 -0500

    Add Server Reload
    This Feature Restart the Server and show changes

--- Proyecto/server.py
+++ Proyecto/server.py
@@ -1,10 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from sys import exit
 from pyramid.paster import get_app
+from tornado.autoreload import add_reload_hook, start
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.options import options, define, parse_command_line
 from tornado.wsgi import WSGIContainer
 
+__autor__ = "William Mendez"
+__version__ = "0.006"
+
 define('port', type= int, default=8080)
+
+server = u"El servidor de desarrollo está iniciando en la url http://127.0.0.1: " + str(options.port)
+
+def auto_reload_hook():
+    print  server
 
 if __name__ == "__main__":
 	parse_command_line()
@@ -12,6 +25,12 @@ if __name__ == "__main__":
 	container = WSGIContainer(app)
 	http_server = HTTPServer(container)
 	http_server.listen(options.port)
-	IOLoop.instance().start()
-
-
+	add_reload_hook(auto_reload_hook)
+    start()
+    print server
+    try:
+        IOLoop.instance().start()
+    except KeyboardInterrupt:
+        print ""
+        print "Saliendo"
+        exit(0)
